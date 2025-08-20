f409ad898bdf857ac6a016c1e4e3461b442ee7fa
ankitnandwani <ankit.nandwani@dpworld.com>
2025-08-21 02:35:14 +0530

    gine

--- goldbees_etf.py
+++ goldbees_etf.py
@@ -30,7 +30,7 @@ st.title("📊 GOLDBEES ETF")
 
 API_KEY = "kdYNchen1BKmeQbK22ingVtEDmd2sph8jKcDNKzf"
 APPLICATION_ID = "dev_zeHSphjh"
-CALLBACK_URL = "https%3A%2F%2Fwww.goldbeesetf.streamlit.app"
+CALLBACK_URL = "https%3A%2F%2Fgoldbeesetf.streamlit.app"
 
 # Initialize session state
 if "access_token" not in st.session_state:
@@ -46,7 +46,7 @@ if auth_token and not st.session_state.access_token:
 
 if not auth_token:
     # Show login button if user not authenticated
-    st.info("mayebe")
+    st.info("no chanze")
     login_url = f"https://flow.rupeezy.in?applicationId={APPLICATION_ID}"
     st.markdown(
         f'<a href="{login_url}" target="_self">'
