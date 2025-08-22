747f8229f1f76735fc447ba3382de974a9a1e4f9
ankitnandwani <ankit.nandwani@dpworld.com>
2025-08-23 01:50:50 +0530

    buy

--- goldbees_etf.py
+++ goldbees_etf.py
@@ -1,4 +1,5 @@
 import json
+import math
 from datetime import datetime, timedelta, UTC
 
 import streamlit as st
@@ -24,6 +25,40 @@ def load_symbol_to_instrument_key_map(json_file="complete.json"):
             symbol_map[inst["trading_symbol"]] = inst["instrument_key"]
 
     return symbol_map
+
+
+def buy(buy_price):
+    min_investment = 10000
+    quantity = max(1, math.ceil(min_investment / buy_price))
+
+    # Display order details
+    st.subheader("🛒 Buy Order details")
+    st.markdown(f"""
+            **Instrument Token:** `{"GOLDBEES"}`    
+            **Quantity:** `{quantity}`  
+            **Order Value:** `₹{quantity * buy_price}`  
+            **Price:** `₹{buy_price}`
+            """)
+
+    api_response = client.place_order(exchange= Constants.ExchangeTypes.NSE_EQUITY, token=ETF_TOKEN,
+                        transaction_type=Constants.TransactionSides.BUY, product=Constants.ProductTypes.MTF,
+                        variety=Constants.VarietyTypes.REGULAR_LIMIT_ORDER,
+                        quantity=quantity, price=buy_price,
+                        validity=Constants.ValidityTypes.AFTER_MARKET)
+
+    st.success(f"✅ Buy order placed successfully: {api_response}")
+
+def get_buy_price():
+    start = datetime.now(UTC) - timedelta(days=1)
+    to = datetime.now(UTC)
+    hist = client.historical_candles(exchange=Constants.ExchangeTypes.NSE_EQUITY, token=ETF_TOKEN, to=to, start=start,
+                                     resolution=Constants.Resolutions.DAY)
+    close_price = hist['c'][0]
+    st.info("close_price : " + str(close_price))
+    buy_p = close_price - 0.20
+    st.info("buy_price : " + str(buy_p))
+    return buy_p
+
 
 
 
@@ -63,12 +98,10 @@ else:
             client = VortexAPI(API_KEY, APPLICATION_ID)
             client.exchange_token(st.session_state.access_token)
             orders = client.orders(limit=20, offset=1)
+            buy_rate = get_buy_price()
+            buy(buy_rate)
 
-            start = datetime.now(UTC) - timedelta(days=1)
-            to = datetime.now(UTC)
-            hist = client.historical_candles(exchange=Constants.ExchangeTypes.NSE_EQUITY, token=ETF_TOKEN, to=to, start=start, resolution=Constants.Resolutions.DAY)
-            st.info("hist : " + str(hist))
-            close_price = hist['c'][0]
-            st.info("close_price : " + str(close_price))
+
+
         except Exception as e:
             st.error(f"Something went wrong: {e}")
