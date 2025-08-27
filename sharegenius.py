706475ce044a4510ac7d2fce8d3e0c6682473ad5
ankitnandwani <ankit.nandwani@dpworld.com>
2025-08-28 00:39:26 +0530

    debug1

--- sharegenius.py
+++ sharegenius.py
@@ -55,9 +55,9 @@ def get_last_n_closes(instrument_token, n=20, days_buffer=60):
     st.info("hist : " + str(hist))
     closes = hist['c']
     st.info("closes : " + str(closes))
-    closerev = closes[::-1]
-    st.info("closerev : " + str(closerev))
-    clos20 = closerev[:n]
+    # closerev = closes[::-1]
+    # st.info("closerev : " + str(closerev))
+    clos20 = closes[:n]
     st.info("clos20 : " + str(clos20))
     return closes[:n] if len(closes) >= n else []
 
