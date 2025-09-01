95576172620824f85b7ea1b78a80aeca55be2d1c
Ankit.Nandwani <ankit.nandwani@dpworld.com>
2025-09-01 21:15:27 +0530

    positions

--- sharegenius.py
+++ sharegenius.py
@@ -271,6 +271,8 @@ else:
             last_trading_price = get_ltp()
             portfolio = client.holdings()
             st.info("portfolio : " + str(portfolio))
+            positions = client.positions()
+            st.info("positions : " + str(positions))
             existing_orders = client.orders(limit=50, offset=1)
             existing_holdings, executed_order_tokens = get_current_portfolio()
             st.info("existing_holdings : " + str(existing_holdings) + " executed_order_tokens : " + str(
