cc0faf353a4e0b673854d4b4310a7897ef0baef0
ankitnandwani <ankit.nandwani@dpworld.com>
2025-09-07 02:15:23 +0530

    debug

--- sharegenius.py
+++ sharegenius.py
@@ -175,7 +175,9 @@ def sell(instrument_key, ltp):
 
 
 def get_current_portfolio():
+    st.info("ok")
     existing_holds = {item["token"] for item in portfolio["data"]["net"]}
+    st.info("existing_holds : " + str(existing_holds))
     return existing_holds
 
 
