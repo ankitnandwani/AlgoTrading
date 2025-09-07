df9b6df05f091d218da55ad9e7186126083e995f
ankitnandwani <ankit.nandwani@dpworld.com>
2025-09-07 21:49:11 +0530

    fix

--- sharegenius.py
+++ sharegenius.py
@@ -232,7 +232,7 @@ def sell_or_take_delivery():
         if deviation >= 3.14:
             sell(instrument_key, ltp, quantity)
 
-        matched_rows = [row for row in all_logs if str(row["Instrument Token"]) == str(instrument_key)]
+        matched_rows = [row for row in all_logs if str(row["Instrument Key"]) == str(instrument_key)]
         if matched_rows:
             order_date_str = matched_rows[0]["Order date"]  # Assuming first column header is "Timestamp"
             order_date = datetime.strptime(order_date_str, "%Y-%m-%d").date()
@@ -271,7 +271,6 @@ else:
             token_resp = client.exchange_token(auth_token)
             token = token_resp["data"]["access_token"]
             etf, jewel, nifty, all_logs = google_auth()
-            st.info("all_logs : " + str(all_logs))
             symbol_to_key = load_symbol_to_instrument_key_map()
             last_trading_price = get_ltp()
             positions = client.positions()
