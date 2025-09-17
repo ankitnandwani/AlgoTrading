d20c0b29b123dfd149d0d443f84624635877bbcc
Ankit.Nandwani <ankit.nandwani@dpworld.com>
2025-09-17 22:58:22 +0530

    funds available check

--- nifty_shop.py
+++ nifty_shop.py
@@ -383,13 +383,11 @@ else:
             user_api = upstox_client.UserApi(api_client)
 
             funds_resp = user_api.get_user_fund_margin(api_version)
-            st.info("funds_resp : " + str(funds_resp))
-            fundsdata = funds_resp.data
-            st.info("fundsdata : " + str(fundsdata))
-            fundsdataequity = funds_resp.data['equity']
-            st.info("fundsdataequity : " + str(fundsdataequity))
             funds = funds_resp.data['equity'].available_margin
             st.info("Available funds : " + str(funds))
+            if funds < 40000:
+                st.error("Gareeb pase daal! Exiting 🚨🚨🚨")
+                st.stop()
 
             portfolio = portfolio_api.get_holdings(api_version)
             existing_orders = order_apiv1.get_order_book(api_version=api_version)
