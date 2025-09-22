5ba8c5448d10d81258dd8d470ca76d3df2b3a99e
Ankit.Nandwani <ankit.nandwani@dpworld.com>
2025-09-22 15:19:57 +0530

    debug4

--- nifty_shop.py
+++ nifty_shop.py
@@ -84,6 +84,7 @@ def compute_top5_nifty_below_ma(is_rsi, stock_list):
         st.info("sym : " + str(sym))
         try:
             instrument_key = symbol_to_key.get(sym)
+            st.info("instrument_key : " + str(instrument_key))
             if not instrument_key:
                 continue
 
