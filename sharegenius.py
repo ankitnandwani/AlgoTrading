b94c2cf245db333ab18f2240238dcaa58db8d78b
Ankit.Nandwani <ankit.nandwani@dpworld.com>
2025-09-24 17:33:35 +0530

    debug3

--- sharegenius.py
+++ sharegenius.py
@@ -243,6 +243,7 @@ def sell_or_take_delivery():
         deviation = ((ltp - avg_buy_price) / avg_buy_price) * 100
 
         matched_rows = [row for row in all_logs if str(row["Instrument Key"]) == str(instrument_key)]
+        st.info("matched_rows : " + str(matched_rows))
         if matched_rows:
             order_date_str = matched_rows[0]["Order date"]  # Assuming first column header is "Timestamp"
             order_date = datetime.strptime(order_date_str, "%Y-%m-%d").date()
