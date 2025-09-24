c2499c62c5dd007b859a08084ad523321cf7c73d
Ankit.Nandwani <ankit.nandwani@dpworld.com>
2025-09-24 23:42:35 +0530

    update nse json

--- nifty_shop.py
+++ nifty_shop.py
@@ -37,9 +37,6 @@ def get_last_n_closes(instrument_key, n, days_buffer):
 
 # 🛠 Helper: Get live LTP
 def get_ltp():
-    st.info("ADANIPOWER ltp")
-    resp = quote_api.get_ltp(instrument_key="NSE_EQ|INE814H01029")
-    st.info("resp : " + str(resp))
     instrument_tokens = [
         symbol_to_key.get(symbol)
         for symbol in all_products
